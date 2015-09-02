2d8255f7a3e59fb6834c150067027fee6990d7bc
Ronalds Sovas <sovas@greenbox.lv>
2015-09-02 14:44:11 +0300

    added seperated view for methods view

--- src/controllers/ApiDemoController.php
+++ src/controllers/ApiDemoController.php
@@ -10,10 +10,31 @@ use \Config;
 
 class ApiDemoController extends \Shivergard\ApiDemo\CrudController {
 
-	public $model = "\Shivergard\ApiDemo\Methods";
+    public $model = "\Shivergard\ApiDemo\Methods";
 
-	public function init(){
-		return view('api-demo::api-demo');
-	}
+    public function init(){
+        return view('api-demo::api-demo');
+    }
+
+    /**
+     * Display the specified resource.
+     * @param  int  $iddd @return Response
+     */
+    public function show($id){
+        $modelName = $this->model;
+        $list = $modelName::find($id);
+        $view = view('api-demo::methods.view' , array(
+                'list' => $list,
+                'fields' => $this->getAllColumnsNames($list),
+                'controller' => $this->getClassName(true),
+        ));
+
+        if (isset($this->layout) && $this->layout){
+            return View($this->layout , array('content' => $view));
+        }else{
+            return $view;
+        }
+        
+    }
 
 }
