17629840710b73b718f85ac04227f82a48d88990
Ronalds Sovas <sovas@greenbox.lv>
2015-10-13 10:13:28 +0300

    fix how is executed method

--- src/controllers/ApiController.php
+++ src/controllers/ApiController.php
@@ -9,23 +9,23 @@ use \Shivergard\ApiDemo\Operations;
 
 class ApiController extends \Shivergard\ApiDemo\PackageController {
 
-	//empty constructor
+    //empty constructor
     public function __construct(){}
 
-	public function apiCall($method){
-		$return = array('status' => 0);
+    public function apiCall($method){
+        $return = array('status' => 0);
 
-		$op = Operations::where('name' , $method);
+        $op = Operations::where('name' , $method);
 
-		if ($op->count() > 0){
-			$op->first();
-			$class = $op->class_path;
-			$method = $op->class_method;
-			$classObject = new $class();
-			$return = $classObject->$class_method(Request::all());
-		}
+        if ($op->count() > 0){
+            $op->first();
+            $class = $op->class_path;
+            $method = $op->class_method;
+            $classObject = new $class();
+            $return = $classObject->{$class_method}(Request::all());
+        }
 
-		return \Response::json($return);
-	}
+        return \Response::json($return);
+    }
 
 }
